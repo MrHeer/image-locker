8f97c9bd4f72a9e14ccfcbbeb4738b523e6f0a62
He Linming <hlm52pk@163.com>
2024-03-29 18:06:12 +0800

    refactor: clean base64 functions

--- src/lib/base64.ts
+++ src/lib/base64.ts
@@ -1,14 +1,12 @@
 function base64ToBuffer(base64: string) {
-  return Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+  return Uint8Array.from(atob(base64), (char) => char.charCodeAt(0));
 }
 
 function bufferToBase64(buffer: ArrayBuffer) {
-  let binary = "";
   const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
+  const binary = Array.from(bytes, (byte) => String.fromCharCode(byte)).join(
+    "",
+  );
   return btoa(binary);
 }
 
